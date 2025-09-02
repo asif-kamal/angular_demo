77cb2f2a8613de4b5030e905254a94ca48ce5b7f
asif-kamal <asif.kamal1489@gmail.com>
2025-09-02 13:40:38 -0500

    objects as input/appropriate typing

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -15,20 +15,25 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
+  // @Input({ required: true }) id!: string;
   // @Input({ required: true }) avatar!: string; //decorator
   // @Input({ required: true }) name!: string;   //decorater
-  avatar = input.required<string>(); //signal
-  name = input.required<string>(); //signal
+  // avatar = input.required<string>(); //signal
+  // name = input.required<string>(); //signal
+  @Input({ required: true }) userObject!: {
+    id: string;
+    avatar: string;
+    name: string;
+  };
+  @Output()
+  select = new EventEmitter<string>();
 
-  @Output() select = new EventEmitter<string>();
-
-  imagePath = computed(() => 'assets/users/' + this.avatar());
+  imagePath = computed(() => 'assets/users/' + this.userObject.avatar);
   // get imagePath() {
   //   return 'assets/users/' + this.avatar;
   // }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.userObject.id);
   }
 }
