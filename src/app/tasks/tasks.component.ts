5a3669779db94d1aca741e5a27221ecb36258cef
asif-kamal <asif.kamal1489@gmail.com>
2025-09-03 14:54:44 -0500

    dependency injection

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -15,7 +16,11 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   isAddingTask = false;
 
-  get selectedUserTasks() {}
+  constructor(private tasksService: TasksService) {} //dependency injecion in Angular
+
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.userId);
+  }
 
   onCompleteTask(id: string) {}
 
